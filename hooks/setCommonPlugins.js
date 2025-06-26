d7adda654e2f5ec15dfcaf584e8e178ada299f37
kobenguyent <7845001+kobenguyent@users.noreply.github.com>
2025-06-26 15:18:49 +0200

    fix: setCommonPlugins no deprecation warning with codeceptjs 3.7.x

--- hooks/setCommonPlugins.js
+++ hooks/setCommonPlugins.js
@@ -4,9 +4,16 @@ module.exports = function() {
 
   config.addHook(cfg => {
     if (!cfg.plugins) cfg.plugins = {};
-    cfg.plugins.tryTo = cfg.plugins.tryTo || { enabled: true };
+
+    try {
+      require('codeceptjs/effects')
+    } catch (err) {
+      console.log(err.message);
+      cfg.plugins.tryTo = cfg.plugins.tryTo || { enabled: true };
+      cfg.plugins.retryTo = cfg.plugins.retryTo || { enabled: true };
+    }
+
     cfg.plugins.retryFailedStep = cfg.plugins.retryFailedStep || { enabled: true };
-    cfg.plugins.retryTo = cfg.plugins.retryTo || { enabled: true };
     cfg.plugins.eachElement = cfg.plugins.eachElement || { enabled: true };
     cfg.plugins.pauseOnFail = cfg.plugins.pauseOnFail || {};
     cfg.plugins.screenshotOnFail = cfg.plugins.screenshotOnFail || {};
